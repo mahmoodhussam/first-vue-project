2a0908f9690c250c6ca6bb2aacb88d7a5ffc013f
Mahmoud Abbas <mahmoodhussam@gmail.com>
2022-07-05 14:55:14 +0300

    Some Changes

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -44,7 +44,9 @@ export default {
     length: Number,
   },
   data() {
-    return {};
+    return {
+      test: "",
+    };
   },
   emits: ["handleNextQuestion", "handleAnswerSelected", "reset"],
 };
