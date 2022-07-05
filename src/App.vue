b26b96a8f7545401025a28da6bcd0feb8fb516c8
Ayyoub Jadoo <ayyoubjadoo@gmail.com>
2022-07-05 14:35:18 +0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,158 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="title">Quiz App</h1>
+  <Question
+    :question="currentQuestion"
+    :answerd="answerd"
+    @handleNextQuestion="handleNextQuestion"
+    @handleAnswerSelected="handleAnswerSelected"
+    @reset="reset"
+    :finshed="finshed"
+    :score="score"
+    :length="questions.length"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Question from "./components/Question.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Question,
+  },
+  data() {
+    return {
+      currQuestion: 0,
+      answerd: "",
+      finshed: false,
+      event: null,
+      score: 0,
+      questions: [
+        {
+          question:
+            "What do you call a computer on a network that requests files from another computer?",
+          answer: 0,
+          options: ["A client", "A host", "A router", "A web server"],
+        },
+        {
+          question:
+            "Hardware devices that are not part of the main computer system and are often added later to the system.",
+          answer: 0,
+          options: ["Peripheral", "Clip art", "Highlight", "Execute"],
+        },
+        {
+          question:
+            "The main computer that stores the files that can be sent to computers that are networked together is:",
+          answer: 3,
+          options: ["Clip art", "Mother board", "Peripheral", "File server"],
+        },
+        {
+          question: "How can you catch a computer virus?",
+          answer: 2,
+          options: [
+            "Sending e-mail messages",
+            "Using a laptop during the winter",
+            "Opening e-mail attachments",
+            "Shopping on-line",
+          ],
+        },
+        {
+          question: "Google (www.google.com) is a:",
+          answer: 0,
+          options: [
+            "Search Engine",
+            "Number in Math",
+            "Directory of images",
+            "Chat service on the web",
+          ],
+        },
+        {
+          question: "Which is not an Internet protocol?",
+          answer: 2,
+          options: ["HTTP", "FTP", "STP", "IP"],
+        },
+        {
+          question: "Which of the following is not a valid domain name?",
+          answer: 0,
+          options: [
+            "www.yahoo.com",
+            "www.yahoo.co.uk",
+            "www.com.yahoo",
+            "www.yahoo.co.in",
+          ],
+        },
+        {
+          question: "AOL stands for:",
+          answer: 3,
+          options: [
+            "Arranged Outer Line",
+            "America Over LAN",
+            "Audio Over LAN",
+            "America On-line",
+          ],
+        },
+        {
+          question: "Another name for a computer chip is:",
+          answer: 1,
+          options: ["Execute", "Micro chip", "Microprocessor", "Select"],
+        },
+        {
+          question: "'www' stands for:",
+          answer: 0,
+          options: ["World Wide Web", "World Wide Wares", "World Wide Wait", "World Wide War"],
+        },
+      ],
+    };
+  },
+  methods: {
+    handleNextQuestion() {
+      if (this.answerd == this.questions[this.currQuestion].answer) {
+        this.score++;
+      }
+      console.log("score", this.score);
+      this.currQuestion++;
+      this.answerd = "";
+      this.event.target.checked = false;
+      if (this.currQuestion == this.questions.length) {
+        this.finshed = true;
+      }
+    },
+    handleAnswerSelected(event) {
+      this.answerd = event.target.value;
+      this.event = event;
+    },
+
+    reset() {
+      this.answerd = "";
+      this.finshed = false;
+      this.currQuestion = 0;
+      this.score = 0;
+    },
+  },
+  computed: {
+    currentQuestion() {
+      return this.questions.find((ques, index) => index === this.currQuestion);
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f1faee;
   margin-top: 60px;
+}
+body {
+  background-color: #457b9d;
+}
+.title {
+  margin-bottom: 3rem;
 }
 </style>
---
+++ src/components/Question.vue
@@ -0,0 +1,126 @@
+<template>
+  <section class="question-container" v-if="!finshed">
+    <h2 class="question-title">{{ question.question }}</h2>
+    <ul class="question-options-container">
+      <li
+        v-for="(option, index) in question.options"
+        :key="index"
+        class="question-option"
+      >
+        <input
+          type="radio"
+          :id="index"
+          :value="index"
+          name="question"
+          class="question-option-input"
+          @change="$emit('handleAnswerSelected', $event)"
+        />
+        <label :for="index" class="question-option-title">{{ option }}</label>
+      </li>
+    </ul>
+    <button
+      :class="answerd ? 'button' : 'disabledButton'"
+      :disabled="answerd ? false : true"
+      @click="$emit('handleNextQuestion')"
+    >
+      SELECT AN OPTION
+    </button>
+  </section>
+  <section v-else>
+    <h2>Score {{ score }} from {{ length }}</h2>
+    <button class="returnButton" @click="$emit('reset')">Return</button>
+  </section>
+  <!-- <h2>{{ question }}</h2> -->
+</template>
+
+<script>
+export default {
+  name: "QuestionT",
+  props: {
+    question: Object,
+    answerd: String,
+    finshed: Boolean,
+    score: Number,
+    length: Number,
+  },
+  data() {
+    return {};
+  },
+  emits: ["handleNextQuestion", "handleAnswerSelected", "reset"],
+};
+</script>
+
+
+<style>
+.question-container {
+  margin: auto;
+  text-align: start;
+  width: 75%;
+  border: 1px solid #fff;
+  padding: 2rem 2rem 2rem 4rem;
+}
+
+.question-options-container {
+  list-style-type: none;
+  text-align: start;
+}
+
+.question-title {
+  margin-bottom: 1.5rem;
+}
+
+.question-option {
+  background-color: #f1faee;
+  color: #457b9d;
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  padding: 1rem;
+}
+
+.question-option-title {
+  cursor: pointer;
+}
+
+.question-option-input {
+  margin-right: 10px;
+}
+
+.button {
+  font-size: 1.5rem;
+  color: #fff;
+  padding: 1rem;
+  border: 0;
+  border: 1px solid #f1faee;
+  background-color: transparent;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+.disabledButton {
+  font-size: 1.5rem;
+  color: #fff;
+  padding: 1rem;
+  border: 0;
+  border: 1px solid #f1faee;
+  background-color: transparent;
+  margin-top: 1rem;
+  opacity: 0.5;
+  /* cursor: pointer; */
+}
+
+.returnButton {
+  font-size: 2rem;
+  color: #fff;
+  padding: 1rem;
+  border: 0;
+  border: 1px solid #f1faee;
+  background-color: transparent;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+
+.returnButton:hover {
+  background-color: #f1faee;
+  color: #457b9d;
+}
+</style>
